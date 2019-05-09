beee7365f4e4f1a7ea3ccbfbca1cf2ddaddc0560
Douglas Campbell <dacampbelltbone@gmail.com>
2019-05-08 23:25:17 -0400

    initial commit; First styling attempt. Social Card MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import HeaderContainer from '../HeaderComponents/HeaderContainer';
+// import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+// import FooterContainer from '../FooterComponents/Footer';
+
 import './Card.css';
+
+
+const CardContainer = () => {
+    return (
+        <div>
+            <HeaderContainer />
+            <CardContent />
+        </div>
+    );
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,22 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div>
+            <a href="https://reactjs.org" target="_blank">
+            <CardBanner />
+            <div className="content">
+            <h4>Get Started with React</h4>
+            <p>React makes it painless to create interactive UIs.
+                Design simple views for each state in your application.
+                <p>reactjs.org</p>
+            </p>
+            </div>
+            </a>
+        </div>
+    );
+}
+
+export default CardContent;
